95a641e2461d1bb1708bf80a5651b56696e2647f
co.abu <anizomxoja@gmail.com>
2022-07-26 11:25:22 +0500

    Initial Commit

---
+++ StoryBoardContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardContacts
+//
+//  Created by Avaz Mukhitdinov on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
